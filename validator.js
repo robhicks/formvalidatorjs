0c0eb563114a433d8f48dd6907cd6e63b434bc22
Rob Hicks <rob@hixfamily.org>
2019-05-13 10:35:05 -0600

    packageize the lib and add eslint

--- validator.js
+++ validator.js
@@ -1,45 +1,45 @@
-var settings = {
-  errored_input_classname: "errored",
-  error_text_classname: "input-error",
-  error_text_data_attribute: "data-error-text"
+let settings = {
+  errored_input_classname: 'errored',
+  error_text_classname: 'input-error',
+  error_text_data_attribute: 'data-error-text'
 };
 
 export default function checkForm(form, settings_override) {
 
-  if(settings_override) {
+  if (settings_override) {
     settings = Object.assign(settings, settings_override);
   }
-  
+
   resetForm(form);
-  
-  var errorsFound = false,
-      requiredInputs = form.querySelectorAll("input[required], textarea[required], select[required]"),
-      re = email_regex = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 
-for(var i=0; i < requiredInputs.length; i++) {
+  let errorsFound = false,
+    requiredInputs = form.querySelectorAll('input[required], textarea[required], select[required]'),
+    re = email_regex = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 
-      var input = requiredInputs[i],
-          val = input.value;
+  for (let i = 0; i < requiredInputs.length; i++) {
 
-  if(!val) {
-    createError(input);
-    errorsFound = true;
-  } else if(input.type === "email" && re.test(val) === false) {
-    createError(input);
-    errorsFound = true;
+    const input = requiredInputs[i],
+      val = input.value;
+
+    if (!val) {
+      createError(input);
+      errorsFound = true;
+    } else if (input.type === 'email' && re.test(val) === false) {
+      createError(input);
+      errorsFound = true;
+    }
   }
-}
-  
-  if(errorsFound) {
-      return false;
+
+  if (errorsFound) {
+    return false;
   }
 
   return true;
 }
 
 export function createError(input) {
-  var errorMessage = input.getAttribute(settings.error_text_data_attribute),
-      errorMessageNode = document.createElement("span");
+  const errorMessage = input.getAttribute(settings.error_text_data_attribute),
+    errorMessageNode = document.createElement('span');
 
   input.classList.add(settings.errored_input_classname);
   errorMessageNode.classList.add(settings.error_text_classname);
@@ -49,17 +49,17 @@ export function createError(input) {
 }
 
 export function resetForm(form) {
-  var erroredInputs = form.querySelectorAll("." + settings.errored_input_classname),
-      errorMessageNodes = form.querySelectorAll("." + settings.error_text_classname);
+  const erroredInputs = form.querySelectorAll(`.${settings.errored_input_classname}`),
+    errorMessageNodes = form.querySelectorAll(`.${settings.error_text_classname}`);
 
-  for(var j=0; j < erroredInputs.length; j++) {
-      var input = erroredInputs[j];
-      input.classList.remove(settings.errored_input_classname);
+  for (let j = 0; j < erroredInputs.length; j++) {
+    const input = erroredInputs[j];
+    input.classList.remove(settings.errored_input_classname);
   }
-  
-  for(var k=0; k < errorMessageNodes.length; k++) {
-      var errorMessageNode = errorMessageNodes[k];
-      errorMessageNode.parentNode.removeChild(errorMessageNode);
+
+  for (let k = 0; k < errorMessageNodes.length; k++) {
+    const errorMessageNode = errorMessageNodes[k];
+    errorMessageNode.parentNode.removeChild(errorMessageNode);
   }
 
 }
